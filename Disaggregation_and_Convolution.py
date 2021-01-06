c764b05eefb3f65ad93822145092f957d075b248
JNogales <jonathan.nogales@tnc.org>
2021-01-06 08:24:00 -0500

    Version-1.0

--- Disaggregation_and_Convolution.py
+++ Disaggregation_and_Convolution.py
@@ -1,24 +1,3 @@
-# Desagregación de salidas anuales multianuales InVEST en series anuales
-
-Para el análisis ROI se requieren resultados distribuidos en el tiempo con resolución anual. Dado que los modelos InVEST son de respuesta promedio de largo plazo, se utiliza una función logística para distribuir en el periodo de tiempo de análisis el resultado obtenido con la modelación. En este apartado se presenta la función que realiza el cálculo.
-
-La distribución del resultado InVEST en el tiempo se realizará utilizando una función logística. Dado que el resultado de flujo base InVEST lo presenta originalmente en milímetros, en este paso se propone transformar en m3. Para esto la respuesta de InVEST debe ser multiplicada por el tramaño de la cuenca.
-
-* El parámetro de porcentaje de beneficio en el tiempo t=0 se calcula considerando el porcentaje de beneficio en t=0 de cada actividad del portafolio mediante un promedio ponderado por el área.
-
-<img src="https://latex.codecogs.com/gif.latex?w&space;=&space;\frac{W_{max}}{1&space;&plus;&space;(\frac{W_{max}}{W_{o}}&space;-&space;1)exp(-rt)&space;}" title="w = \frac{W_{max}}{1 + (\frac{W_{max}}{W_{o}} - 1)exp(-rt) }" /></a>
-
-
-* El parámetro r se calcula considerando el tiempo en alcanzar el 100% del beneficio del portafolio. Este tiempo se calculo mediante un promedio ponderado por el área del tiempo para cada actividad.
-
-<img src="https://latex.codecogs.com/gif.latex?Benefit_{t=0}&space;=&space;\frac{\sum_{n}^{i=1}A_{i}*Benefit_{i}}{\sum_{n^{i=1}A_{i}}}" title="Benefit_{t=0} = \frac{\sum_{n}^{i=1}A_{i}*Benefit_{i}}{\sum_{n^{i=1}A_{i}}}" /></a>
-
-<img src="https://latex.codecogs.com/gif.latex?t_{MaxBenefit}&space;=&space;\frac{\sum_{n}^{i=1}A_{i}*t_{MaxBenefit_i}}{\sum_{n^{i=1}A_{i}}}" title="t_{MaxBenefit} = \frac{\sum_{n}^{i=1}A_{i}*t_{MaxBenefit_i}}{\sum_{n^{i=1}A_{i}}}" /></a>
-
-En el esquema de cálculo, se realiza la distribución con la función logística para el beneficio marginal de cada portafolio y al final se realiza una convolución de los resultados considerando el desplazamiento anual de cada portafolio. El desarrollador de software deberá comprender el esquema de cálculo formulado a partir de este ejemplo y generalizarlo para su implementación
-
-## Código
-´´´
 # -*- coding: utf-8 -*-
 # Import Packages
 import numpy as np
@@ -87,12 +66,7 @@ def Desaggregation_BaU_NBS(PathProject):
     '''
     Results_BaU.to_csv(os.path.join(PathProject,'02-OUTPUTS_BaU.csv'), index_label='Time')
     Results_NBS.to_csv(os.path.join(PathProject,'02-OUTPUTS_NBS.csv'), index_label='Time')
-	
-´´´
-## Tester 
-´´´
-# Path
+
+# terter
 PathProject = r'Z:\Box Sync\01-TNC-ThinkPad-P51\28-Project-WaterFund_App\02-Productos-Intermedios\Python_Convolution\Project'
-# Desaggregation
 Desaggregation_BaU_NBS(PathProject)
-´´´
